8e2228bfc8861e0d00478732117d3d456823bfcf
genjian.li <genjian.li@70capital.com>
2021-07-15 17:17:12 +0800

    hft improve

---
+++ examples/snap_basic_stats.py
@@ -0,0 +1,18 @@
+from hft_signal_maker.hft_pipeline import HftPipeline, HftContext
+
+
+def calculate_snap_basic_bar(cxt):
+    assert isinstance(cxt, HftContext)
+    snap = cxt.get_snap(dimension_fix=True, time_flag_freq='1min', only_trade_time=True)
+    return snap
+
+
+pipeline = HftPipeline(include_snap=True)
+pipeline.add_block_step(calculate_snap_basic_bar)
+
+
+if __name__ == '__main__':
+    result = pipeline.compute(start_ds='20210608', end_ds='20210608', universe=['000001.SZ', '000637.SZ'])
+    # result = pipeline.compute(start_ds='20200101', end_ds='20200110', universe='StockA')
+    print(result)
+
--- hft_pipeline.py
+++ hft_pipeline.py
@@ -1,12 +1,35 @@
 from team_api import date_util as du
 from team_api.data.data_getter import api
-from dataapi.stock.cudf_market import get_cudf_transaction, get_cudf_snapshot, get_cudf_order_sh, get_cudf_order_sz
-import cudf
 import logbook
 import pandas as pd
+import numba
 
 
 pd.options.display.max_columns = 20
+
+
+@numba.njit
+def _numba_ts_align(ts: int, freq_second: int):
+    second = ts % 100
+    minute = (ts // 100) % 100
+    hour = ts // 10000
+    if second > 60 - freq_second:
+        minute, second = minute + 1, 0
+    else:
+        second = (second - 1) // freq_second + 1
+    if minute >= 60:
+        hour, minute = hour + 1, 0
+    return hour * 10000 + minute * 100 + second
+
+
+def _time_flag(cdf_time, freq):
+    if freq.endswith('s'):
+        resample_second = int(freq.replace('second', '').replace('s', ''))
+    elif freq.endswith('min') or freq.endswith('m'):
+        resample_second = int(freq.replace('min', '').replace('m', '')) * 60
+    else:
+        resample_second = 3
+    return cdf_time.map(lambda x: _numba_ts_align(x // 1000, resample_second))
 
 
 class HftContext:
@@ -18,19 +41,89 @@ class HftContext:
         self.snap_data = None
         self.result_data = []
 
-    def get_trans(self, dimension_fix=True, with_minute_flag=False, exclude_auction=False, exclude_cancel=True):
-        if dimension_fix:
-            self.trans_data['price'] = self.trans_data['price'] / 10000
-        if with_minute_flag:
-            self.trans_data['minute_flag'] = self.trans_data['time'].map(lambda x: (x - 1) // 100000 + 1)
-        if exclude_auction:
-            self.trans_data = self.trans_data[self.trans_data['time'] >= 93000000]
-        if exclude_cancel:
-            self.trans_data = self.trans_data[self.trans_data['transType'] != 0]
-        return self.trans_data
+    def get_trans(self, dimension_fix=True, time_flag_freq='1min', exclude_auction=False, exclude_cancel=True):
+        """
+        获取逐笔成交数据
 
-    def get_snap(self):
-        pass
+        :param dimension_fix: 是否要修正价格数据量纲，默认True
+        :param time_flag_freq: 数据中'time_flag'字段的采样频次，默认为1min，可选3s/10s/1min/10min/30min
+        :param exclude_auction: 是否剔除集合竞价阶段数据
+        :param exclude_cancel: 是否剔除取消单
+        :return: cudf.DataFrame
+        """
+        assert self.trans_data is not None
+        res = self.trans_data.copy()
+        res['time_flag'] = _time_flag(res['time'], time_flag_freq)
+        if dimension_fix:
+            res['price'] = res['price'] / 10000
+        if exclude_auction:
+            res = res[res['time'] >= 93000000]
+        if exclude_cancel:
+            res = res[res['transType'] != 0]
+        return res
+
+    def get_snap(self, dimension_fix=True, time_flag_freq='3s', only_trade_time=False, exclude_auction=False,
+                 exclude_post_trading=False):
+        """
+        获取快照截面数据
+
+        :param dimension_fix: 是否要修正价格数据量纲，默认True
+        :param time_flag_freq: 数据中'time_flag'字段的采样频次，默认为1min，可选3s/10s/1min/10min/30min
+        :param only_trade_time: 是否只包含交易时间数据，默认为False
+        :param exclude_auction: 是否剔除集合竞价数据，默认为False
+        :param exclude_post_trading: 是否剔除盘后数据，默认为False
+        :return:
+        """
+        assert self.snap_data is not None
+        res = self.snap_data.copy()
+        res['time_flag'] = _time_flag(res['time'], time_flag_freq)
+        res = res.sort_values('time').drop_duplicates(subset=['code', 'time_flag'], keep='last')
+        if dimension_fix:
+            fix_fields = ['last', 'min', 'max', 'open', 'high', 'low']
+            for i in range(1, 11):
+                fix_fields.append(f'ask{i}')
+                fix_fields.append(f'bid{i}')
+            for field in fix_fields:
+                res[field] = res[field] / 10000
+        if exclude_auction:
+            res = res[res['time'] >= 93000000]
+        if exclude_post_trading:
+            res = res[res['time'] <= 150000000]
+        if only_trade_time:
+            res = res[(res['time'] >= 93000000) & (res['time'] <= 113000000)
+                      | (res['time'] >= 130000000) & (res['time'] <= 150000000)]
+        return res
+
+    def get_snap_tensor(self, dimension_fix=True, time_flag_freq='3s', only_trade_time=False, exclude_auction=False,
+                        exclude_post_trading=False, ):
+        """
+        获取快照截面tensor数据
+
+        :param dimension_fix: 是否要修正价格数据量纲，默认True
+        :param time_flag_freq: 数据中'time_flag'字段的采样频次，默认为1min，可选3s/10s/1min/10min/30min
+        :param only_trade_time: 是否只包含交易时间数据，默认为False
+        :param exclude_auction: 是否剔除集合竞价数据，默认为False
+        :param exclude_post_trading: 是否剔除盘后数据，默认为False
+
+        :return: (截面tensor => cupy.Array, {'codes': 股票列表, 'fields': 字段列表, 'times': 时间戳列表} => dict)
+        """
+        import cupy as cp
+        snap = self.get_snap(dimension_fix=dimension_fix, time_flag_freq=time_flag_freq, only_trade_time=only_trade_time,
+                             exclude_auction=exclude_auction, exclude_post_trading=exclude_post_trading).set_index(
+            ['time_flag', 'code'])
+        columns = [c for c in snap.columns if c not in ('ds', 'code', 'time_flag')]
+        times = None
+        codes = None
+        snap_unstack = snap.unstack()
+        tensor_result = []
+        for c in columns:
+            tensor_result.append(cp.asarray(snap_unstack[c].as_gpu_matrix()))
+            if times is None:
+                times = [f'{self.ds} {t//10000:02d}:{t//100%100:02d}:{t%100:02d}' for t in snap_unstack[c].index.to_pandas()]
+            if codes is None:
+                codes = list(snap_unstack[c].columns)
+        tensor_result = cp.stack(tensor_result)
+        return tensor_result, {'fields': columns, 'codes': codes, 'times': times}
 
     def get_order(self):
         pass
@@ -50,8 +143,20 @@ class HftPipeline:
         self.minute_flag = minute_flag
         self._steps = []
 
-    def compute(self, start_ds, end_ds, universe='StockA', n_code_partition=1):
-        assert n_code_partition == 1, NotImplementedError()
+    def compute(self, start_ds, end_ds, universe='StockA', n_code_block=1):
+        """
+        计算当前高频因子值
+
+        :param start_ds: 开始时间
+        :param end_ds: 结束时间
+        :param universe: 股票池
+        :param n_code_block: 进行block计算时，code切分的数量
+        :return:
+        """
+        from dataapi.stock.cudf_market import get_cudf_transaction, get_cudf_snapshot, get_cudf_order_sh, \
+            get_cudf_order_sz
+        import cudf
+        assert n_code_block == 1, NotImplementedError()
         trading_days = du.get_between_date(start_ds, end_ds)
         context = HftContext()
         result = []
@@ -64,10 +169,10 @@ class HftPipeline:
                 code_list = list(api.universe(ds, universe).reset_index()['code'].unique())
             if self.include_snap:
                 logbook.info(f'start load snap of {ds}')
-                snap = get_cudf_snapshot(ds, code=code_list, source='rough_merge')
+                snap = get_cudf_snapshot(ds, code=code_list, source='rough_merge_v2', ns_time=False)
                 context.snap_data = snap
             if self.include_trans:
-                trans = get_cudf_transaction(ds, code=code_list, source='rough_merge_v0')
+                trans = get_cudf_transaction(ds, code=code_list, source='rough_merge_v0', ns_time=False)
                 context.trans_data = trans
             if self.include_order:
                 raise NotImplementedError()
@@ -77,7 +182,7 @@ class HftPipeline:
                     res = func(context)
                     res = res.reset_index()
                     res['ds'] = ds
-                    context.result_data.append(res.set_index(['ds', 'code', 'minute_flag']))
+                    context.result_data.append(res.set_index(['ds', 'code', 'time_flag']))
                 else:
                     raise NotImplementedError()
             ds_res = cudf.concat(context.result_data).sort_index().to_pandas()
@@ -86,7 +191,17 @@ class HftPipeline:
         return pd.concat(result)
 
     def add_block_step(self, func, **kwargs):
+        """
+        新增block步骤，block步骤为定义在一组code及特定时间范围内的聚合运算
+
+        :param func: HftContext => cudf.DataFrame(index with (code, time_freq))。计算当前block对应的对齐数据
+        :param kwargs:
+        :return:
+        """
         self._steps.append(('block', func, kwargs))
+
+    def add_cross_step(self, func, **kwargs):
+        self._steps.append(('cross', func, kwargs))
 
 
 if __name__ == '__main__':
