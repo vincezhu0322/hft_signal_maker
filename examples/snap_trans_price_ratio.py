cb9506aa664c61b5f357f319746e6d2cbde2c745
朱汶轩 <wenxuan.zhu@70capital.com>
2021-07-23 14:46:35 +0800

    add change to fill_time_flag

--- examples/snap_trans_price_ratio.py
+++ examples/snap_trans_price_ratio.py
@@ -3,9 +3,9 @@ from hft_signal_maker.hft_pipeline import HftPipeline
 
 
 def calculate_snap_trans_price_ratio(cxt):
-    trans = cxt.get_trans(time_flag_freq='1min', exclude_auction=False, exclude_cancel=True)
+    trans = cxt.get_trans(time_flag_freq='1min', only_trade_time=True)
     trans = trans[['ds', 'code', 'time', 'bsFlag', 'price']]
-    snapshot = cxt.get_snap(time_flag_freq='1min', exclude_auction=True, exclude_post_trading=True)
+    snapshot = cxt.get_snap(time_flag_freq='1min', only_trade_time=True)
     snapshot = snapshot[['ds', 'code', 'time', 'bid1', 'ask1']]
     snapshot.drop_duplicates(subset=['ds', 'code', 'time_flag'], keep='last', inplace=True)
     snapshot['price'] = (snapshot.bid1 + snapshot.ask1)
@@ -19,10 +19,7 @@ def calculate_snap_trans_price_ratio(cxt):
     return snapshot
 
 
-pipeline = HftPipeline('1min_snap_price_ratio', include_trans=True, include_snap=True)
-pipeline.add_block_step(calculate_snap_trans_price_ratio)
-
-
 if __name__ == '__main__':
+    pipeline = HftPipeline('1min_snap_price_ratio', include_trans=True, include_snap=True)
+    pipeline.add_block_step(calculate_snap_trans_price_ratio)
     data = pipeline.compute('20210101', '20210201')
-    print(data)
