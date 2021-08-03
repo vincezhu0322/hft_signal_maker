6a1baed2c2cb6282803d55038b215c974d1176b9
朱汶轩 <wenxuan.zhu@70capital.com>
2021-08-03 18:24:12 +0800

    add change to _ts_align

--- numba_util.py
+++ numba_util.py
@@ -6,12 +6,18 @@ def _ts_align(ts: int, freq_second: int):
     second = ts % 100
     minute = (ts // 100) % 100
     hour = ts // 10000
-    if second > 60 - freq_second:
+    if second >= 60 - freq_second:
         minute, second = minute + 1, 0
     else:
         second = ((second - 1) // freq_second + 1) * freq_second
-    if minute >= 60:
+    if freq_second > 60:
+        freq_minute = freq_second / 60
+    else:
+        freq_minute = 1
+    if minute >= 60 - freq_minute:
         hour, minute = hour + 1, 0
+    else:
+        minute = ((minute - 1) // freq_minute + 1) * freq_minute
     return hour * 10000 + minute * 100 + second
 
 
